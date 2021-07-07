ce49349e044785ecb19e8e7d2dc8336c77d8f220
RistoReborn91 <chris.lee.designer@gmail.com>
2021-07-07 17:27:37 -0400

    Update CalcLinkedMax.js

--- CalcLinkedMax.js
+++ CalcLinkedMax.js
@@ -1,3 +1,20 @@
-function CalcLinkedMax() {
-	
+function CalcLinkedMax(e, _localFieldName, _extLibName, _extFieldName) {
+	var tasklib = libByName(_extLibName);
+	var links = tasklib.linksTo(e);
+	var max = 0;
+
+
+	for (var i = 0; i < links.length; i++) {
+
+	  var current_value = links[i].field(_extFieldName);
+	  
+	  if (max < _current_value)
+		  max = _current_value;
+	  
+	}
+
+
+	e.set(_localFieldName, max);
+
+
 }
