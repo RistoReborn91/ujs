cfd5db68de0772ce3500d690b065a42c399335f7
RistoReborn91 <chris.lee.designer@gmail.com>
2021-07-12 20:17:23 -0400

    Update CalcLinkedMax.js

--- CalcLinkedMax.js
+++ CalcLinkedMax.js
@@ -1,14 +1,15 @@
-function CalcLinkedMax(e, _localFieldName, _extLibName, _extFieldName) {
+function CalcLinkedMax(e, _localFieldName, _extLibName, _extLinkFieldName, _extValueFieldName) {
 	
 	log("====================================");
-	log("e: The current entry().");
-	log("_localFieldName: The name of the local library field that will hold the result.");
+	log("_extValueFieldName: The name of the linked library field from which results are calculated.");
+	log("_extLinkFieldName: Specifies the name of the link field in case there are multiple in the external library.");
 	log("_extLibName: The name of the linked library.");
-	log("_extFieldName: The name of the linked library field from which results are calculated.");
+	log("_localFieldName: The name of the local library field that will hold the result.");
+	log("e: The current entry().");
 	log("====================================");
 
 
-	if (e != null && _localFieldName != null && _extLibName != null && _extFieldName != null) {
+	if (e != null && _localFieldName != null && _extLibName != null && _extValueFieldName != null) {
 		var tasklib = libByName(_extLibName);
 		var links = tasklib.linksTo(e);
 		var max = 0;
@@ -16,12 +17,16 @@ function CalcLinkedMax(e, _localFieldName, _extLibName, _extFieldName) {
 
 		for (var i = 0; i < links.length; i++) {
 
-		  var current_value = links[i].field(_extFieldName);
+		  if (links[i].field(_extLinkFieldName) == e) {
+
+		  var current_value = links[i].field(_extValueFieldName);
 		  
 		  if (max < current_value)
 			  max = current_value;
 		  
-		}
+		  } // if
+		  
+		} //  for loop
 
 
 		e.set(_localFieldName, max);
