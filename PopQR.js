803cad7e011a9f3bf155562b5b58b4978817252e
RistoReborn91 <48331300+RistoReborn91@users.noreply.github.com>
2021-07-18 18:18:57 -0400

    Update PopQR.js

--- PopQR.js
+++ PopQR.js
@@ -1,1 +1,21 @@
+function PopQR(e, f, q) {
+  
+log("====================================");
+log("q: The name of the QR field.");
+log("f: The name of the field from which to copy.");
+log("e: The current entry.");
+log("====================================");
 
+if (e == null || f == null || q == null) {
+  message("Please fill all parameters")
+  cancel();
+} else {
+var i = e.field(f);
+
+
+
+e.set(q, i);
+  
+  message(q + " set to " + i);
+}
+}
