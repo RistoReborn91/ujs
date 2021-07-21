8b17efffb61f526d7691c6f940b710cfbcece82c
RistoReborn91 <48331300+RistoReborn91@users.noreply.github.com>
2021-07-21 11:16:09 -0400

    Update CountLinks.js

--- CountLinks.js
+++ CountLinks.js
@@ -1,29 +1,18 @@
-function CountLinks(e, _extLibName, _extLinkFieldName) {
+function CountLinks(e, _extLibName) {
 	
 	log("====================================");
-	log("_extLinkFieldName: Specifies the name of the link field in case there are multiple in the external library.");
+	// log("_extLinkFieldName: Specifies the name of the link field in case there are multiple in the external library.");
 	log("_extLibName: The name of the linked library.");
 	log("e: The current entry().");
 	log("====================================");
 
 
-	if (e != null && _extLibName != null && _extLinkFieldName != null) {
+	if (e != null && _extLibName != null) {
 		var tasklib = libByName(_extLibName);
 		var links = tasklib.linksTo(e);
-		var result = 0;
-
-
-		for (var i = 0; i < links.length; i++) {
-
-		  // if (links[i].field(_extLinkFieldName) == e)
-                    result++;
-		  
-		 
-		  
-		} //  for loop
-
-
-		return result;
+		return links.length;
 
 	}
 }
+
+
