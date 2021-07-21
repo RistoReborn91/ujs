515dc63b30992c951e45365b29b489ccf64a26e7
RistoReborn91 <48331300+RistoReborn91@users.noreply.github.com>
2021-07-21 10:45:04 -0400

    Update CountLinks.js

--- CountLinks.js
+++ CountLinks.js
@@ -1,10 +1,9 @@
-function CalcLinkedMax(e, _localFieldName, _extLibName, _extLinkFieldName, _extValueFieldName) {
+function CountLinks(e, _extLibName, _extLinkFieldName, _extValueFieldName) {
 	
 	log("====================================");
 	log("_extValueFieldName: The name of the linked library field from which results are calculated.");
 	log("_extLinkFieldName: Specifies the name of the link field in case there are multiple in the external library.");
 	log("_extLibName: The name of the linked library.");
-	log("_localFieldName: The name of the local library field that will hold the result.");
 	log("e: The current entry().");
 	log("====================================");
 
@@ -26,7 +25,7 @@ function CalcLinkedMax(e, _localFieldName, _extLibName, _extLinkFieldName, _extV
 		} //  for loop
 
 
-		e.set(_localFieldName, result);
+		result;
 
 	}
 }
