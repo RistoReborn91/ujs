d0204c3a29740176ad85e127b6113000bb77fffa
RistoReborn91 <48331300+RistoReborn91@users.noreply.github.com>
2021-07-23 12:35:59 -0400

    Update SetOnLink.js

--- SetOnLink.js
+++ SetOnLink.js
@@ -1,4 +1,16 @@
-function SetOnLink(e, lf, v, xl) {
+function SetOnLink(e, f, v, xl) {
 
+log("====================================");
+log("xl: The name of the external library.");
+log("v: The value to which the lf will be set.");
+log("f: The name of the local field.");
+log("e: The current entry().");
+log("====================================");
+
+
+ if (masterLib().title == xl) {
+  e.set(f, v);
+  message(f + " was set to " + v);
+ }
 
 } // SetOnLink
