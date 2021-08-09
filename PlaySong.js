babe5cb6b209f70737e73310f5994a775c8a2721
RistoReborn91 <48331300+RistoReborn91@users.noreply.github.com>
2021-08-09 11:16:00 -0400

    Update PlaySong.js

--- PlaySong.js
+++ PlaySong.js
@@ -2,9 +2,9 @@ function PlaySong(e, _field) {
 
 var bgm = e.field(_field);
 
-if (bgm.length == 0) {
+if (bgm.length == 0)
   message("This entry does not have any " + _field + " links.");
-} else {
+else {
 var bgm1 = bgm[0];
 var q = bgm1.field("◻ ID"); 
 
