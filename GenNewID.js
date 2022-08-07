480be05a96c83c0b7944cf6b438be1f62f75eff4
RistoReborn91 <48331300+RistoReborn91@users.noreply.github.com>
2022-08-07 05:17:58 -0400

    Update GenNewID.js

--- GenNewID.js
+++ GenNewID.js
@@ -1,5 +1,7 @@
 function GenNewID(e2) {
 
+// Can be set as a Create Entry Pre-Save Trigger if the I field is not set to be unique.
+
 var e = lib().entries();
 var max = e.length;
 
@@ -15,7 +17,7 @@ var newid = max + 1;
 
 e2.set("I", newid);
 
-
+message("Entry index is now # " + newid);
 
 
 }
